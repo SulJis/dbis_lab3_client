de7eaf7d9cc19eff3b3118d8e130048414ab9161
Maxim Slipchenko <44846411+SulJis@users.noreply.github.com>
2021-05-24 11:45:03 +0300

    Delete db directory

--- db/db_models/__init__.py
+++
@@ -1,6 +0,0 @@
-from db.db_models.user import User
-from db.db_models.list import List
-from db.db_models.note import Note
-from db.db_models.label import Label
-
-__all__ = ["User", "List", "Note", "Label"]
